fceb501413c6195ba878d64f3670edf7f581bd64
Peter <peterwong0803@gmail.com>
2022-10-22 12:42:40 +0800

    Update layout of notification

--- src/components/NotificationView.js
+++ src/components/NotificationView.js
@@ -1,22 +1,38 @@
 import React from "react";
 
-export const NotificationView = ({ message, type }) => {
+export const NotificationView = ({ title, message, type, date }) => {
     return (
         <div
             style={{
                 display: "flex",
-                flexDirection: "row",
-                height: "80px",
+                flexDirection: "column",
+                // height: "80px",
                 backgroundColor: "white",
                 borderWidth: "3px 3px 3px 22px",
                 borderColor: type === "success" ? "green" : "red",
                 borderStyle: "solid",
                 borderRadius: "10px",
-                alignItems: "center",
-                padding: "0px 20px 0px 20px",
+                padding: "20px",
+                margin: "10px 0px 10px 0px",
             }}
         >
-            {message}
+            <div
+                style={{
+                    fontSize: "25px",
+                    fontWeight: "bold",
+                    color: "#101010",
+                }}
+            >
+                {title}
+            </div>
+            <div dangerouslySetInnerHTML={{ __html: message }} />
+            <div
+                style={{
+                    color: '#a9a9a9'
+                }}
+            >
+                {date}
+            </div>
         </div>
     );
 }
