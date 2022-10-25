33ad927945e4b22b62cea107ad7fc06700be0249
weiyee731 <weiyee731@gmail.com>
2022-10-25 17:07:03 +0800

    footer design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,12 @@ import {
     FooterText
 } from "./FooterStyles";
 import { useTheme } from "@mui/material";
-
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CallIcon from '@mui/icons-material/Call';
+import EmailIcon from '@mui/icons-material/Email';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import { Typography, Grid, } from '@mui/material';
 const Footer = () => {
     const theme = useTheme()
     return (
@@ -18,7 +23,7 @@ const Footer = () => {
                 <Row>
                     <Column>
                         <Heading>关于我们</Heading>
-                        <FooterText>雅威国际物流提供专业的从中国至东马的海运物流服务。初衷以简单，实惠，安全和快速为主。</FooterText>
+                        <FooterText>雅威国际物流提供专业从中国至东马的海运物流服务。初衷以简单，实惠，安心交托为主。</FooterText>
                     </Column>
                     <Column>
                         <Heading>相关资讯</Heading>
@@ -30,23 +35,35 @@ const Footer = () => {
                     </Column>
                     <Column>
                         <Heading>客服服务</Heading>
+                        <FooterText>
+                            <Grid container spacing={2} >
+                                <Grid item xs={2} ><LocationOnIcon /></Grid>
+                                <Grid item xs={10}>Stapok</Grid>
+                            </Grid>
+                        </FooterText>
+                        <FooterText>
+                            <Grid container spacing={2} >
+                                <Grid item xs={2} ><CallIcon /></Grid>
+                                <Grid item xs={10}>011 - 5555 2203</Grid>
+                            </Grid>
+                        </FooterText>
                         <FooterLink href="#">
-                            Address
+                            <Grid container spacing={2} >
+                                <Grid item xs={2} ><EmailIcon /></Grid>
+                                <Grid item xs={10}>yawei_helpdesk@gmail.com</Grid>
+                            </Grid>
                         </FooterLink>
+                        <FooterText>
+                            <Grid container spacing={2} >
+                                <Grid item xs={2} ><AccessTimeIcon /></Grid>
+                                <Grid item xs={10}>Mon - Sat : 9:00am - 6:00pm</Grid>
+                            </Grid>
+                        </FooterText>
                         <FooterLink href="#">
-                            Contact
-                        </FooterLink>
-                        <FooterLink href="#">
-                            Email
-                        </FooterLink>
-                        <FooterLink href="#">
-                            WechatID
-                        </FooterLink>
-                        <FooterLink href="#">
-                            Operation Time
-                        </FooterLink>
-                        <FooterLink href="#">
-                            Facebook
+                            <Grid container spacing={2} >
+                                <Grid item xs={2} ><FacebookIcon /></Grid>
+                                <Grid item xs={10}>Yourway Logistic Sdn Bhd</Grid>
+                            </Grid>
                         </FooterLink>
                     </Column>
                 </Row>
